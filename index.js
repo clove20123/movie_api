fd6ee2811549970f3818ae6290d0d199b3f71cd5
clove20123 <79578949+clove20123@users.noreply.github.com>
2021-04-29 11:38:05 +0200

    Added middleware and error handling.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+morgan = require('morgan');
+const bodyParser = require('body-parser'),
+methodOverride = require('method-override');
 
 let topMovies = [
 {
@@ -69,6 +72,19 @@ app.get('/movies', (req, res) => {
 
 app.use(express.static('public'));
 
+app.use(morgan('common'));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 // listen for requests
 app.listen(8080, () => {
